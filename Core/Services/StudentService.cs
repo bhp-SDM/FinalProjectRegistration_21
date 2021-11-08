85ecb3ae497a2b59f6f8f815a9fb5f80f737a31c
BentHPedersen <benthpedersen@mail.com>
2021-11-08 11:18:27 +0100

    AddStudent: throws exception on invalid student

--- Core/Services/StudentService.cs
+++ Core/Services/StudentService.cs
@@ -30,7 +30,26 @@ namespace Core.Services
         /// <param name="s">The Student to add</param>
         public void AddStudent(Student s)
         {
+            ThrowIfInvalidStudent(s);
             studentRepo.Add(s);
+        }
+
+        private void ThrowIfInvalidStudent(Student s)
+        {
+            if (s == null)
+                throw new ArgumentException("Student is missing");
+            if (s.Id <= 0)
+                throw new ArgumentException("Invalid Id");
+            if (s.Name == null || s.Name == "")
+                throw new ArgumentException("Name is missing or empty");
+            if (s.Address == null || s.Address == "")
+                throw new ArgumentException("Address is missing or empty");
+            if (s.Zipcode < 1 || s.Zipcode > 9999)
+                throw new ArgumentException("Invalid Zipcode");
+            if (s.City == null || s.City == "")
+                throw new ArgumentException("City is missing or empty");
+            if (s.Email != null && s.Email == "")
+                throw new ArgumentException("Email is empty");
         }
 
         public List<Student> GetAllStudents()
--- XUnitTestProject/StudentServiceTest.cs
+++ XUnitTestProject/StudentServiceTest.cs
@@ -13,7 +13,7 @@ namespace XUnitTestProject
         /// Test for creating ad valid StudentService.
         /// </summary>
         [Fact]
-        public void CreateStudentServicevalidRepositoty()
+        public void CreateStudentService_ValidRepositoty()
         {
             Mock<IStudentRepository> mock = new Mock<IStudentRepository>();
             IStudentService service = null;
@@ -26,7 +26,7 @@ namespace XUnitTestProject
         /// Test for trowing exception if the repository is null.
         /// </summary>
         [Fact]
-        public void CreateStudentServiceRepositoryIsNullThrowsException()
+        public void CreateStudentService_RepositoryIsNull_ThrowsException()
         {
             IStudentService service = null;
             var ex = Assert.Throws<ArgumentException>(() => service = new StudentService(null));
@@ -38,16 +38,16 @@ namespace XUnitTestProject
         /// Test for adding a valid student to the repository.
         /// The student email is optional.
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="name"></param>
-        /// <param name="address"></param>
-        /// <param name="zipcode"></param>
-        /// <param name="city"></param>
-        /// <param name="email"></param>
+        /// <param name = "id" ></ param >
+        /// < param name="name"></param>
+        /// <param name = "address" ></ param >
+        /// < param name="zipcode"></param>
+        /// <param name = "city" ></ param >
+        /// < param name="email"></param>
         [Theory]
         [InlineData(1, "Name", "Address", 1234, "City", null)]
         [InlineData(1, "Name", "Address", 1234, "City", "abc@mail.com")]
-        public void AddStudentIsValidStudent(int id, string name, string address, int zipcode, string city, string email)
+        public void AddStudent_ValidStudent(int id, string name, string address, int zipcode, string city, string email)
         {
             Mock<IStudentRepository> mock = new Mock<IStudentRepository>();
             IStudentService service = new StudentService(mock.Object);
@@ -57,25 +57,59 @@ namespace XUnitTestProject
 
             mock.Verify(x => x.Add(s), Times.Once);
         }
+        /// <summary>
+        /// Test for throwing ArgumentException if Student object contains invalid properties.
+        /// 1. id must be greater than 0.
+        /// 2. Name cannot be null or empty.
+        /// 3. Address cannot be null or empty.
+        /// 4. Zipcode between [1-9999]
+        /// 5. City cannot be null or empty.
+        /// 6. Email is optional, but cannot be empty.
+        /// </summary>
+        /// <param name="id">test id</param>
+        /// <param name="name">test name</param>
+        /// <param name="address">test address</param>
+        /// <param name="zipcode">test zipcode</param>
+        /// <param name="city">test city</param>
+        /// <param name="email">test email</param>
+        /// <param name="errorMessage">error message for exceptino</param>
+        [Theory]
+        [InlineData(0, "Name", "Address", 1234, "City", "e@mail.dk", "Invalid Id")]         // Id = 0
+        [InlineData(-1, "Name", "Address", 1234, "City", null, "Invalid Id")]               // Id is negative
+        [InlineData(1, null, "Address", 1234, "City", null, "Name is missing or empty")]    // Name is null
+        [InlineData(1, "", "Address", 1234, "City", null, "Name is missing or empty")]      // Name is empty
+        [InlineData(1, "Name", null, 1234, "City", null, "Address is missing or empty")]    // Address is null
+        [InlineData(1, "Name", "", 0, "City", null, "Address is missing or empty")]         // Address is empty         
+        [InlineData(1, "Name", "Address", 0, "City", null, "Invalid Zipcode")]              // Zipcode too low
+        [InlineData(1, "Name", "Address", 10000, "City", null, "Invalid Zipcode")]          // Zipcode too high
+        [InlineData(1, "Name", "Address", 1234, null, null, "City is missing or empty")]    // City is null
+        [InlineData(1, "Name", "Address", 1234, "", null, "City is missing or empty")]      // City is empty
+        [InlineData(1, "Name", "Address", 1234, "City", "", "Email is empty")]              // Email is empty
 
-        //[Theory]
-        //[InlineData(0, "Name", "Address", 1234, "City")]
-        //[InlineData(-1, "Name", "Address", 1234, "City")]
-        //[InlineData(1, null, "Address", 1234, "City")]
-        //[InlineData(1, "", "Address", 1234, "City")]
-        //[InlineData(1, "Name", null, 1234, "City")]
-        //[InlineData(1, "Name", "", 0, "City")]
-        //[InlineData(1, "Name", "Address", 10000, "City")]
-        //[InlineData(1, "Name", "Address", 1234, null)]
-        //[InlineData(1, "Name", "Address", 1234, "")]
-        //public void AddStudentInvalidStudentThrowsException(int id, string name, string address, int zipcode, string city)
-        //{
-        //    Mock<IStudentRepository> mock = new Mock<IStudentRepository>();
-        //    IStudentService service = new StudentService(mock.Object);
-        //    Student s = new Student(id, name, address, zipcode, city, null);
-        //    var ex = Assert.Throws<ArgumentException>(() => service.AddStudent(s));
+        public void AddStudent_InvalidStudent_ThrowsException(int id, string name, string address, int zipcode, string city, string email, string errorMessage)
+        {
+            Mock<IStudentRepository> mock = new Mock<IStudentRepository>();
+            IStudentService service = new StudentService(mock.Object);
+            Student s = new Student(id, name, address, zipcode, city, email);
             
-        //    mock.Verify((x) => x.Add(s), Times.Never);
-        //}
+            var ex = Assert.Throws<ArgumentException>(() => service.AddStudent(s));
+            
+            Assert.Equal(errorMessage, ex.Message);
+            mock.Verify((x) => x.Add(s), Times.Never);
+        }
+        /// <summary>
+        /// Test throwing exception if Student object is null.
+        /// </summary>
+        [Fact]
+        public void AddStudent_StudentIsNull_ThrowsException()
+        {
+            Mock<IStudentRepository> mock = new Mock<IStudentRepository>();
+            IStudentService service = new StudentService(mock.Object);
+
+            var ex = Assert.Throws<ArgumentException>(() => service.AddStudent(null));
+            
+            Assert.Equal("Student is missing", ex.Message);
+            mock.Verify(x => x.Add(null), Times.Never);
+        }
     }
 }
